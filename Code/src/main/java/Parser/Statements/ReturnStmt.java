5fd03d6c3e62de5cebbc0a4be747ac03a9480b00
Marius Kleppe Larnøy <mariusklarnoy@gmail.com>
2020-03-25 20:43:48 +0100

    removed semicolon from classes

--- Code/src/main/java/Parser/Statements/ReturnStmt.java
+++ Code/src/main/java/Parser/Statements/ReturnStmt.java
@@ -4,26 +4,23 @@ import Parser.Expressions.*;
 public class ReturnStmt implements Stmt {
     public final String returnString;
     public final Exp right;
-    public final String semicolon;
 
-    public ReturnStmt(final String returnString, final Exp right, final String semicolon){
+    public ReturnStmt(final String returnString, final Exp right){
         this.returnString = returnString;
         this.right = right;
-        this.semicolon = semicolon;
     }
 
     public boolean equals(final Object other){
         if (other instanceof  ReturnStmt){
             final ReturnStmt otherRtnStmt = (ReturnStmt)other;
             return (returnString.equals(otherRtnStmt.returnString) &&
-                    right.equals(otherRtnStmt.right) &&
-                    semicolon.equals(otherRtnStmt.semicolon));
+                    right.equals(otherRtnStmt.right));
         } else return false;
     }
 
     public String toString() {
         return String.format(this.getClass().getSimpleName() +
-                " (" + returnString + " " + right + " " + semicolon + ")");
+                " (" + returnString + " " + right + ")");
     }
 }
 
