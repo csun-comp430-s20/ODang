8b1fc172be75369be48cf49715d5578eb424f945
Marius Kleppe Larnøy <mariusklarnoy@gmail.com>
2020-05-15 19:32:24 +0200

    updated ODang.java

--- Code/src/main/java/ODang.java
+++ Code/src/main/java/ODang.java
@@ -1,72 +1,65 @@
-import Tokenizer.Tokenizer;
-import Parser.Parser;
+import Tokenizer.*;
+import Parser.*;
 import Parser.Declarations.Decl;
-import Typechecker.Typechecker;
-import CodeGenerator.CodeGenerator;
+import Typechecker.*;
+import CodeGenerator.*;
 
 import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
 public class ODang {
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to ODang");
-        System.out.println("Created by: \nCharles Dang, " +
+        System.out.println("Created by: Charles Dang, " +
                 "Marius Kleppe Larnøy and Giovanni Orozco\n");
-
-        while(true) {
+        String fileName = "";
+        while (true) {
             try {
-                final String programString = readFile(new Scanner(System.in));
-                if (programString.equalsIgnoreCase("q"))
+                System.out.println("Enter name of file to compile (<filename>.odang)\n" +
+                        "Enter 'quit' to exit");
+                Scanner scanner = new Scanner(System.in);
+                final String userInput = scanner.nextLine();
+                if(userInput.equalsIgnoreCase("quit"))
                     break;
-
-                compile(programString);
-                System.out.println("Compilation complete!\nFile saved as <filename>.js");
-                break;
-            } catch (Exception e) {
-                System.out.println(e.getLocalizedMessage());
+                else {
+                    fileName = userInput.split("\\.")[0];
+                    final String extension = userInput.split("\\.")[1];
+                    if (!extension.equalsIgnoreCase("odang"))
+                        break;
+                    final String tokenizerInput = readFile(userInput);
+                    compile(tokenizerInput);
+                    break;
+                }
+            } catch (final Exception e) {
+                System.out.println(e.getClass().getSimpleName() +": " + e.getMessage());
+                continue;
             }
+
         }
+        System.out.printf("Compilation complete!\nFile saved as %s.js", fileName);
 
     }
 
-    public static String readFile(final Scanner scanner) throws IOException, InvalidFileNameException {
-
-        System.out.println("Specify file to compile (<filename>.odang) \nEnter 'q' to exit\n >");
-        String inputFileName = scanner.nextLine();
-
-        if (inputFileName.equalsIgnoreCase("q"))
-            return inputFileName;
-        else {
-
-            final String extension = inputFileName.split("\\.")[1];
-            if (!extension.equalsIgnoreCase("odang"))
-                throw new InvalidFileNameException("Invalid file extension: " + extension);
-
-            final File file = new File(inputFileName);
-            final BufferedReader br = new BufferedReader(new FileReader(file));
-            String tokenizerInput = "";
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                tokenizerInput += line;
-            }
-            br.close();
-            scanner.close();
-            return tokenizerInput;
+    public static String readFile(final String fileName) throws IOException {
+        final File file = new File(fileName);
+        final BufferedReader br = new BufferedReader(new FileReader(file));
+        String tokenizerInput = "";
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            tokenizerInput += line;
         }
+        br.close();
+        return tokenizerInput;
     }
-    public static void compile(final String input) {
-        try {
-            final List<Decl> AST = (
-                    new Parser(new Tokenizer(input).tokenize()).parseProgram()
-            );
-            new Typechecker(AST).typecheckProgram();
-            final CodeGenerator codeGen = new CodeGenerator(AST);
+    public static void compile(final String inputString) throws TokenizerException, ParseException,
+                                                        IllTypedException, CodeGeneratorException {
+        final List<Decl> parsedProgram = new Parser(new Tokenizer(inputString).tokenize()).parseProgram();
+        new Typechecker(parsedProgram).typecheckProgram();
+        final CodeGenerator codeGen = new CodeGenerator(parsedProgram);
+        //TODO output file
 
-            //TODO finish generation
-        } catch(final Exception e) {
-            System.out.println(e.getLocalizedMessage());
-        }
     }
 }
